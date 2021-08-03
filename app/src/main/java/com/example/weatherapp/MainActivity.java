a422f30517ced2bcc3ee6a78ba51193b3532fd7c
john kaizer <johnkaiser964@gmail.com>
2021-08-03 15:28:51 +0300

    WeatherApp. Done with java

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String APP_ID="902ed46a9a3798beb0afea825eaa21a8";
+    final String APP_ID="";
     final  String Weather_URL="https://home.openweathermap.org/data/2.5/weather";
     final long MIN_TIME= 5000;
     final float Min_Distance= 1000;
